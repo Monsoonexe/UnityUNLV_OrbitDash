128203594c71a2d8b7d91ac48d731f21e4b3018d
Monsoonexe <43690629+Monsoonexe@users.noreply.github.com>
2018-11-09 11:32:21 -0800

    Foundation Assets for Player and Orbiter
    press space to lerp square towards circle

---
+++ Assets/Scripts/OrbiterController.cs
@@ -0,0 +1,63 @@
+﻿//Author: Richard Osborn
+//Date 11/9/18
+//description
+//attach this monoBehavior to an object, and it will orbit around the orbitParent transform
+
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class OrbiterController : MonoBehaviour {
+    public Transform orbitParent;
+    public GameObject orbitingObject;
+    public float orbitSpeed;
+    public float orbitalRadius = 2.0f;
+    public bool orbitClockwise = true;
+
+	// Use this for initialization
+	void Start () {
+        if(orbitParent == null)
+        {
+            Debug.LogError("ERROR! orbitParent not set on " + this.gameObject.name);
+            //set at runtime
+        }
+
+        orbitingObject.transform.position = (orbitingObject.transform.position - orbitParent.position).normalized * orbitalRadius + orbitParent.position;
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        orbitingObject.transform.RotateAround(orbitParent.position, Vector3.forward, orbitSpeed * Time.deltaTime);
+		
+	}
+
+    public Vector3 GetOrbiterLocalPosition()
+    {
+        return orbitingObject.transform.position;
+    }
+
+    private void OnGUI()
+    {
+        if (GUILayout.Button("ReverseOrbit"))
+        {
+            ReverseOrbit();
+        }
+    }
+
+    public void ReverseOrbit()
+    {
+        orbitSpeed *= -1;
+
+    }
+
+    private void OnTriggerEnter(Collider col)//does not work! collider not on this object
+    {
+        Debug.Log("OnTriggerEnter()!!!!");
+        if (col.gameObject.CompareTag("Enemy"))//if object's tag in list of objects can destroy....
+        {
+            Destroy(col.gameObject);
+        }
+
+    }
+}
---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,53 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour { 
+    public float moveSpeed = 1.0f;
+    public OrbiterController orbiterController;
+
+    private Vector3 startPosition;
+    private Vector3 targetMovePosition;
+    private bool moving = false;
+
+    //these values used for linear interpolation (lerp)
+    private float moveStartTime;
+    private float movementLength;
+    private float distanceCovered;
+    private float percentOfJourneyCompleted;
+
+
+    // Use this for initialization
+    void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+        if (Input.GetButtonDown("Jump"))
+        {
+            //Debug.Log("Jump pressed!");//print test
+            
+            //for lerping
+            moving = true;
+            targetMovePosition = orbiterController.GetOrbiterLocalPosition();
+            startPosition = this.transform.position;
+            moveStartTime = Time.time;//start
+            movementLength = Vector3.Distance(targetMovePosition, startPosition);//how far do I have to move?
+
+            //this.transform.position = orbiterController.GetOrbiterWorldPosition();//teleport broken
+        }
+        if (moving)
+        {
+            //Debug.Log("MOVE IT!");//print test
+            distanceCovered = ((Time.time - moveStartTime) * moveSpeed);
+            percentOfJourneyCompleted = distanceCovered / movementLength;
+            Debug.Log("StartPos = " + startPosition + " " + "targetMovePosition = " + targetMovePosition + " " + percentOfJourneyCompleted + "%");
+            this.transform.position = Vector3.Lerp(startPosition, targetMovePosition, percentOfJourneyCompleted);
+
+        }
+        
+
+    }
+}

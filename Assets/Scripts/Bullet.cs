faa7b19dd2bd14e19dde0d9816212bf2c5f5a4c0
Monsoonexe <43690629+Monsoonexe@users.noreply.github.com>
2018-11-15 12:12:55 -0800

    added gameplay elements
    added spawner that spawns bullets from random points in a list
    bullets move the direction they are facing
    added new tag for PlayAreaBounds

---
+++ Assets/Scripts/Bullet.cs
@@ -0,0 +1,59 @@
+﻿//Author: Richard Osborn
+//Date 11/11/18
+//Description: Bullet class
+//this object moves forward forever at a constant rate
+//is destroyed when leaving PlayAreaBoundary TriggerVolume
+
+using UnityEngine;
+
+public class Bullet : MonoBehaviour {
+    [SerializeField] private float initialForce = 100.0f;//ex, when shot from a gun
+    [SerializeField] private Vector2 direction;//ex, when shot from a gun
+    //this [attribute] makes private variables visible in the Inspector.
+
+    Bullet()//empty default constructor
+    {
+        ;
+    }
+
+    Bullet(float force)//constructor 
+    {
+        initialForce = force;//
+    }
+
+    //Bullet(float force, Vector3 desiredDirection)//constructor //probably not needed as Instantiate will determine orientation
+    //{
+    //    initialForce = force;//
+    //    direction = desiredDirection;
+    //}
+
+
+    // Use this for initialization
+    void Start () {
+        
+        if(this.transform.rotation.y == 1)
+        {
+            direction = Vector2.left;
+        }
+        else
+        {
+            direction = Vector2.right;
+        }
+        //Debug.Log(this.transform.rotation + " " + direction);
+        this.GetComponent<Rigidbody2D>().AddForce(direction * initialForce, ForceMode2D.Force);//'forward'
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    public void OnTriggerExit2D(Collider2D collision)
+    {
+        //destroy this game object if it leaves the play area
+        if (collision.gameObject.CompareTag("PlayAreaBoundary"))
+        {
+            Destroy(this.gameObject);
+        }
+    }
+}
---
+++ Assets/Scripts/Spawner.cs
@@ -0,0 +1,101 @@
+﻿//Author: Richard Osborn
+//Date 11/11/18
+//Spawns things
+using UnityEngine;
+using System.Collections;
+
+public class Spawner : MonoBehaviour
+{
+    public GameObject[] thingsToSpawn;
+    [Header("Weights not yet implemented.")]
+    public int[] weights; //one thingsToSpawn should have one weight. There should always be more higher weight than lower weighted things
+    public Transform[] spawnPoints;
+    public int maxThingsAliveAtOneTime;
+    public int minDelayBetweenSpawns;
+    public int maxDelayBetweenSpawns;
+    public bool spawning = true;
+
+    public GameObject[] listOfExistingObjects;
+
+    private bool DetermineValidSetup()
+    {
+        bool setupIsValid = true;
+
+        if (thingsToSpawn == null ||thingsToSpawn.Length  < 1)
+        {
+            Debug.LogError("ERROR! There is nothing to spawn!");
+            setupIsValid = false;
+        }
+        if (spawnPoints == null || spawnPoints.Length < 1)
+        {
+            Debug.LogError("ERROR! There are no valid spawn points!");
+            setupIsValid = false;
+            //check children for spawn points and try to assign them. retry
+        }
+
+        return setupIsValid;
+    }
+
+	// Use this for initialization
+	void Start ()
+    {
+        //check for valid setup configuration of thingsToSpawn and spawnPoints
+        if (!DetermineValidSetup())
+        {
+            Debug.Log("Destroying self....");
+            Destroy(this.gameObject);
+        }
+        listOfExistingObjects = new GameObject[maxThingsAliveAtOneTime];
+
+        StartCoroutine(StartSpawning());
+        
+	}
+
+    //counts how many objects in the list are still alive
+    private static int CountAliveObjects(GameObject[] listOfObjects)
+    {
+        int accumulator = 0;
+        foreach(GameObject obj in listOfObjects)
+        {
+            if (obj != null) ++accumulator;
+        }
+
+        return accumulator;        
+    }
+
+    private IEnumerator StartSpawning()
+    {
+        int objectsAlive;//will count how many objects in listOfExistingObjects is not null
+        Transform parent; 
+        while (spawning)//unless told otherwise, do this forever
+        {
+            objectsAlive = CountAliveObjects(listOfExistingObjects);//how many alive objects are there in this list?
+            if (objectsAlive < maxThingsAliveAtOneTime)//do we need to spawn more things?
+            {
+                Debug.Log("Spawning!!!");
+                parent = spawnPoints[(int)Random.Range(0, spawnPoints.Length)];//pick a random parent
+                for(int i = 0; i < listOfExistingObjects.Length; ++i)//look through each element of the list until you find an empty spot
+                {
+                    if(listOfExistingObjects[i] == null)//if this is an empty slot
+                    {
+                        //fill slot with newly instantiated object
+                        listOfExistingObjects[i] = Instantiate(thingsToSpawn[(int)Random.Range(0, thingsToSpawn.Length)], //spawn a random object from this list
+                            parent.position, parent.rotation);//relative to its parent
+                        //Debug.Log(parent.rotation);
+                        break;//only do this once per check
+                    }//end if
+                }//end for
+                
+            }//end if
+            yield return new WaitForSeconds(Random.Range(minDelayBetweenSpawns, maxDelayBetweenSpawns));//randomizes time between spawns
+        }
+        
+    }
+	
+	// Update is called once per frame
+	void Update ()
+    {
+        
+		
+	}
+}
